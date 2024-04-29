8c2af7f92f16ff470772a5c97245bf276b2b20d7
Dars2000 <128153675+Dars2000@users.noreply.github.com>
2024-04-29 15:11:45 -0400

    Update API-Prueba.py

--- API-Prueba.py
+++ API-Prueba.py
@@ -1,23 +1,50 @@
 import uvicorn
 from fastapi import FastAPI
-
-segundos = 10
+import threading
 
 app = FastAPI(
     title="Segundos",
     version="0.0.1",
-    # openapi_url=""
 )
 
-@app.post("/escribir")
-def EscribirSegundos(s: int):
+segundos = None
+lock = threading.Lock()
+actualizacion_reciente = False
+
+def reset_segundos():
     global segundos
+    global lock
+    global actualizacion_reciente
+    lock.acquire()
+    if not actualizacion_reciente:
+        segundos = None
+    actualizacion_reciente = False
+    lock.release()
+
+def start_reset_timer():
+    threading.Timer(2, reset_segundos).start()
+
+@app.post("/escribir")
+def escribir_segundos(s: int):
+    global segundos
+    global lock
+    global actualizacion_reciente
+    lock.acquire()
     segundos = s
+    actualizacion_reciente = True
+    lock.release()
 
 @app.get("/leer")
-def LeerSegundos():
+def leer_segundos():
     global segundos
-    return segundos
+    global lock
+    global actualizacion_reciente
+    lock.acquire()
+    s = segundos
+    actualizacion_reciente = True
+    lock.release()
+    start_reset_timer()
+    return s
 
 if __name__ == "__main__":
-    uvicorn.run("segundos_api:app", host="localhost", port=5030, log_level="info", reload=True)
+    uvicorn.run("API-Prueba:app", host="localhost", port=5030, log_level="info", reload=True)
